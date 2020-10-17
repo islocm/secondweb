d79976ea15ee7724bbe68b83439a7e5e58f384dc
iSLocm <rawcagh1@gmail.com>
2020-10-17 15:23:07 +0500

    new commit

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
+package main
 
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	_ "github.com/lib/pq"
+)
+
+var router *gin.Engine
+
+func main() {
+
+	e := connect()
+	if e != nil {
+		fmt.Println(e.Error())
+		return
+	}
+	fmt.Println("ist work")
+	router = gin.Default()
+	router.GET("/", index)
+	router.Static("/assets", cfg.Assets)
+	router.LoadHTMLFiles(cfg.HTML + "index.html")
+
+	router.Run(cfg.ServerHost + ":" + cfg.ServerPort)
+}
+
+func index(c *gin.Context) {
+	c.HTML(200, "index.html", nil)
+}
